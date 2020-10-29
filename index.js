bbc8b70a1f5b24b8c0cbcaf4b8557e3134b75fbe
eowsley <66394238+eowsley@users.noreply.github.com>
2020-10-29 15:11:25 -0700

    final commit

---
+++ index.js
@@ -0,0 +1,9 @@
+// DEFINE YOUR FUNCTION called rant which accepts a string argument called message.
+// function should print out an uppercased version of message 3 times:
+
+function rant(message) {
+  return message.toUpperCase();
+}
+console.log(rant("I hate bad drivers!"));
+console.log(rant("I hate bad drivers!"));
+console.log(rant("I hate bad drivers!"));
